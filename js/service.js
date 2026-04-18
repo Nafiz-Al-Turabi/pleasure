4018a54f490437ccf9a3aa790ff866665ab2437b
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-04-18 21:10:19 +0600

    Add service page components and scripts for dynamic content loading

---
+++ js/service.js
@@ -0,0 +1,26 @@
+fetch("../components/navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar").innerHTML = data;
+
+    const script = document.createElement("script");
+    script.src = "../js/nav.js";
+    document.body.appendChild(script);
+  });
+
+fetch("../components/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+fetch("../components/service/service-hero.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("service-hero").innerHTML = data;
+  });
+fetch("../components/service/vip-service.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("vip-service").innerHTML = data;
+  });
